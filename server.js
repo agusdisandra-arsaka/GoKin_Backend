e669a3eadc895b50dd763efb2eb9c99f1c3a6f2b
agusdi sandra <agusdi@gmail.com>
2025-11-26 18:46:58 +0700

    update server.js fix

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
+// Connect DB
+const connectDB = require('./db/db');
+
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect DB
+// Connect to MongoDB
 connectDB();
 
 // Routes
@@ -20,5 +24,13 @@ app.use('/api/users', require('./routes/users'));
 const swaggerDocument = YAML.load(__dirname + '/swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Render requires this to respond for health checks
+app.get('/', (req, res) => {
+  res.send("GoKin Backend is running...");
+});
+
+// Server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log('Server running on', PORT));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
